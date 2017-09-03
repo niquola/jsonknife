3d060a08f2345fc1edf5ae9fcbc451bbabdd90e2
yury <andreev_yurij@mail.ru>
2017-09-03 20:52:22 +0300

    include commented

--- jsonknife.c
+++ jsonknife.c
@@ -30,7 +30,7 @@
 #include "utils/json.h"
 #include "utils/jsonb.h"
 #include "utils/timestamp.h"
-#include "utils/varlena.h"
+/*#include "utils/varlena.h"*/
 
 typedef enum MinMax {min, max} MinMax;  
 
